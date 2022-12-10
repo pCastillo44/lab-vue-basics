34372298d9e17d229886ca74beed8f47e0632e64
Patricio Castillo <patri_alcarras@hotmail.com>
2022-12-10 11:13:34 +0100

    Vue basics

---
+++ vue-basics/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  
+  <Header />
+  <p> {{ 2 + 2 }}</p>
+  <p :class="show">Texto condicional</p>
+
+<Footer />
+</template>
+
+<script setup>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+const show = ref('show');
+</script>
+
+<style>
+
+.show {
+  display: block;
+ 
+}
+
+.hidden {
+    display: none;
+  }
+</style>
